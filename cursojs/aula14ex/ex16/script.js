83842aa93241975ecf445f0df16f912e6755e81f
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2025-01-14 02:07:13 -0300

    42

--- cursojs/aula14ex/ex16/script.js
+++ cursojs/aula14ex/ex16/script.js
@@ -1,1 +1,36 @@
-alert("Olá");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", contar);
+
+function contar() {
+  let ini = document.getElementById("txti");
+  let end = document.getElementById("txtf");
+  let step = document.getElementById("txtp");
+  let res = document.getElementById("res");
+  if (
+    ini.value.length == 0 ||
+    end.value.length == 0 ||
+    step.value.length == 0
+  ) {
+    alert("Faltam dados");
+  } else {
+    res.innerHTML = "Contando: ";
+    let i = Number(ini.value);
+    let e = Number(end.value);
+    let s = Number(step.value);
+
+    //Contagem crescente
+    if (i < f) {
+      for (let c = i; c <= e; c += s) {
+        res.innerHTML += ` ${c} \u{1F449}`;
+      }
+    }
+
+    //Contagem decrescente
+    if (i > f) {
+      for (let c = i; c >= e; c -= s) {
+        res.innerHTML += ` ${c} \u{1F449}`;
+      }
+    }
+    res.innerHTML += `\u{1F3C1}`;
+  }
+}
--- cursojs/exercicios/d04/script.js
+++ cursojs/exercicios/d04/script.js
@@ -1,24 +1,16 @@
 var btn = document.getElementById("btn");
 btn.addEventListener("click", calcular);
 
-function deletar() {
-  var tabuadas = document.getElementById("tabu");
-  if (Number(tabuadas.length) <= 2) {
+function deletar(tabuadas) {
+  for (var c = 0; c <= 9; c++) {
     tabuadas.remove(0);
-    tabuadas.remove(1);
-  }
-
-  if (Number(tabuadas.length) >= 10) {
-    for (var c = 1; c <= 10; c++) {
-      tabuadas.remove(c);
-    }
   }
 }
 
 function calcular() {
-  deletar();
-  var num = document.getElementById("txtnum");
   var tabuadas = document.getElementById("tabu");
+  var num = document.getElementById("txtnum");
+  deletar(tabuadas);
   if (tabuadas.length == 0) {
     for (var c = 1; c <= 10; c++) {
       var option = document.createElement("option");
