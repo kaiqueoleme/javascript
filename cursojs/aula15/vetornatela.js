f5b5dd321a54aa16a6c5bad7ea38380f6ed5b7ce
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2025-01-15 01:04:25 -0300

    1

---
+++ cursojs/aula15/vetornatela.js
@@ -0,0 +1,5 @@
+let valores = [8,1,7,4,2,9]
+
+for (var valor in valores) {
+    console.log(valor)
+}
--- cursojs/exercicios/d04/script.js
+++ cursojs/exercicios/d04/script.js
@@ -1,23 +1,19 @@
 var btn = document.getElementById("btn");
 btn.addEventListener("click", calcular);
 
-
-
 function calcular() {
   var tabuadas = document.getElementById("tabu");
   var num = document.getElementById("txtnum");
   var col = document.getElementById("txtcol");
   var ncol = Number(col.value);
 
-  for (var c = 0; c <= ncol - 1; c++) {
-    tabuadas.remove(0);
-  }
-  
+  tabuadas.innerHTML = "";
 
   for (var c = 1; c <= ncol; c++) {
     var option = document.createElement("option");
     var r = Number(num.value) * c;
     option.text = `${num.value} x ${c} = ${r}`;
-    tabuadas.add(option);
+    option.value = `tab${c}`
+    tabuadas.appendChild(option);
   }
 }
