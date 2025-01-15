a360efe8f7da5c72c4c77118d796868e952dc49a
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2025-01-15 00:19:23 -0300

    10

--- cursojs/exercicios/d04/script.js
+++ cursojs/exercicios/d04/script.js
@@ -1,22 +1,23 @@
 var btn = document.getElementById("btn");
 btn.addEventListener("click", calcular);
 
-function deletar(tabuadas) {
-  for (var c = 0; c <= 9; c++) {
-    tabuadas.remove(0);
-  }
-}
+
 
 function calcular() {
   var tabuadas = document.getElementById("tabu");
   var num = document.getElementById("txtnum");
-  deletar(tabuadas);
-  if (tabuadas.length == 0) {
-    for (var c = 1; c <= 10; c++) {
-      var option = document.createElement("option");
-      var r = Number(num.value) * c;
-      option.text = `${num.value} x ${c} = ${r}`;
-      tabuadas.add(option);
-    }
+  var col = document.getElementById("txtcol");
+  var ncol = Number(col.value);
+
+  for (var c = 0; c <= ncol - 1; c++) {
+    tabuadas.remove(0);
+  }
+  
+
+  for (var c = 1; c <= ncol; c++) {
+    var option = document.createElement("option");
+    var r = Number(num.value) * c;
+    option.text = `${num.value} x ${c} = ${r}`;
+    tabuadas.add(option);
   }
 }
