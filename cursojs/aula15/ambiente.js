2b9698a26130cf99addaa959f45c05ef35380c14
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2025-01-19 23:49:16 -0300

    1

--- cursojs/aula15/ambiente.js
+++ cursojs/aula15/ambiente.js
@@ -1,6 +1,15 @@
-let num  = [5,8,2,9,3]
-num.sort()
-num.push(1)
-console.log(num)
-console.log(`O vetor tem ${num.length} posições`)
-console.log(`O primeiro valor do vetor é ${num[0]}`)
+let num = [5, 8, 2, 9, 3];
+num.sort();
+num.push(1);
+console.log(num);
+console.log(`O vetor tem ${num.length} posições`);
+console.log("");
+console.log(`O primeiro valor do vetor é ${num[0]}`);
+console.log("");
+let valor = 6;
+let pos = num.indexOf(valor);
+if (pos == -1) {
+  console.log(`O valor ${valor} não foi encontrado`);
+} else {
+  console.log(`O valor 4 está na posição ${pos}`);
+}
--- cursojs/aula15/vetornatela.js
+++ cursojs/aula15/vetornatela.js
@@ -1,5 +1,5 @@
 let valores = [8,1,7,4,2,9]
 
 for (var valor in valores) {
-    console.log(valor)
+    console.log(`A posição ${valor} tem o valor ${valores[valor]}`)
 }
